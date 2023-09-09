ec42397129f82a8d3fb1b51aa5b0524f3703ed78
shaileshkumar <shailesh.kumar@prodo.in>
2023-09-09 11:11:02 +0530

    api setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import MealList from "../views/MealList.vue";
 const routes = [
   {
     path: "/",
     name: "home",
     component: Home,
   },
+  {
+    path: "/letter/:letter",
+    name: "byLatter",
+    component: MealList,
+  },
 ];
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
---
+++ src/views/MealList.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>MealList</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
